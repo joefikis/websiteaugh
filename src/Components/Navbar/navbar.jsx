2f31fb8171f108b08e204949e315c3b5f54b1dac
JoeFikis <aidanmg608@gmail.com>
2023-09-15 22:38:27 -0500

    augh

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import './Styles/index.css'
 
 function Navbar() {
   return (
-    <div>navbar</div>
+    <>
+    <nav>
+        <div className="navbar-container">
+            <div className="navbar-wrapper">
+                <div className="navbar-logo">
+                    <h1>
+                        LOGO
+                    </h1>
+                </div>
+                <div className="navbar-link-wrapper">
+                    <div className="navbar-links">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+    </>
   )
 }
 
